2a73360ee7b81909db6268cd0113db02059d4513
Zack Cao <zack.cao@gmail.com>
2015-02-05 20:39:38 -0500

    add again

--- CaoSeanCalculator/CaoSeanCalculator/Form1.cs
+++ CaoSeanCalculator/CaoSeanCalculator/Form1.cs
@@ -19,7 +19,7 @@ namespace CaoSeanCalculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World! ");
+            MessageBox.Show("Hello World! again");
         }
     }
 }
